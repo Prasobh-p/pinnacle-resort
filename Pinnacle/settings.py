200014f56c5cd690affcbab8c7795178670cc355
prasobh-p <prasobhgupta007@gmail.com>
2025-08-29 18:00:45 +0530

    Add default DATABASE_URL fallback

--- Pinnacle/settings.py
+++ Pinnacle/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Pinnacle.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
+        default=config('DATABASE_URL', default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')),
         conn_max_age=600
     )
 }
